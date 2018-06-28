3faad693c1a7d7051e7a8b1ecf55f3e7a050586f
Motoki Kobayashi <motoki.kobayashi@studio-umi.jp>
2018-06-28 16:36:57 +0900

    リファクタリング

--- main.php
+++ main.php
@@ -13,21 +13,26 @@ require_once dirname(__FILE__) . '/src/Player.php';
 echo "★☆★☆★☆★☆ ブラックジャックへようこそ ☆★☆★☆★☆★\n";
 echo "ゲームを開始します\n";
 
+// 山札の生成.
 $deck = new Deck();
 
+// プレイヤー、ディーラーの生成.
 $player = new Player('あなた', $deck);
 $dealer = new Player('ディーラー', $deck);
 
+// プレイヤーの初期カードを引く.
 for ($i = 0; $i < 2; $i++) {
   echo $player->choiseCard();
 }
 
+// ディーラーの初期カードを引く.
 for ($i = 0; $i < 2; $i++) {
   echo $dealer->choiseCard($i == 1);
 }
 
 echo "\n";
 
+// プレイヤーの入力に基づきカードを引く.
 do {
   $player_total = $player->getCardsScore();
   echo "{$player->getName()}の現在の得点は${player_total}です\n";
@@ -52,11 +57,12 @@ do {
 
 echo "\n";
 
+// ディーラーが2枚目に引いたカード情報及び得点を表示.
 echo $dealer->getSelectedCard(2);
-
 $dealer_total = $dealer->getCardsScore();
 echo "{$dealer->getName()}の現在の得点は${dealer_total}です\n";
 
+// ディーラーのカードを引く.
 while ($dealer_total <= 17 && !$player->isBurst()) {
   echo $dealer->choiseCard();
   $dealer_total = $dealer->getCardsScore();
@@ -67,6 +73,7 @@ echo "\n";
 echo "{$player->getName()}の得点は${player_total}です\n";
 echo "{$dealer->getName()}の得点は${dealer_total}です\n";
 
+// 勝敗判定.
 if ($player->isBurst() || (!$dealer->isBurst() && $dealer_total > $player_total)) {
   echo "{$dealer->getName()}の勝ちです。";
 }
